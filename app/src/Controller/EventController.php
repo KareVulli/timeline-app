4177d799050a4c199b8c71011055f118a89abbd7
Marvin Helstein <m_helstein@hotmail.com>
2019-03-28 15:27:23 +0200

    Fixed authentication and firewall problems

--- app/src/Controller/EventController.php
+++ app/src/Controller/EventController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Form\EventType;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/api", name="api_events_")
@@ -28,6 +29,7 @@ class EventController extends FOSRestController
 
     /**
      * @Rest\Post("/event", name="save_event")
+     * @IsGranted("ROLE_USER")
      */
     public function postEventAction(Request $request): View
     {
